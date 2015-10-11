c49536d75f52e53814043b8800cdf56af705eafb
Klaas Kliffen <KlaasYK@users.noreply.github.com>
2015-10-11 21:03:48 +0200

    Ass2 working

--- ass2/gencluster.m
+++ ass2/gencluster.m
@@ -3,11 +3,11 @@
 #graphics_toolkit('gnuplot')
 function s = gencluster(p, size)
 
-probs = rand(2*size+1)
+probs = rand(2*size+1);
 map = 0.5*ones(2*size+1,2*size+1);
 
 #mark the center spot as occupied
-probs(size+1,size+1) =  1.0;
+probs(size+1,size+1) =  0;
 
 # Take the center point and move it on top
 current = [size+1,size+1];
@@ -19,44 +19,37 @@ while (rows(current) > 0)
   current(1,:) = [];
   loops = loops + 1;
 
+  x = top(1);
+  y = top(2);
   #Check if current node is not visited
-  if ( map(top(1),top(2)) == 0.5 )
+  if ( map(x,y) == 0.5 )
 
-    if ( probs(top(1),top(2)) > p )
-      map(top(1),top(2)) = 0.0;
-      # Add neighbours
-      if (top(2) > 1)
-        if (top(1) > 1)
-          current(end+1,:) = [top(1)-1,top(2)-1];
-        end
-        current(end+1,:) = [top(1),top(2)-1];
-        if (top(1) < size*2+1)
-          current(end+1,:) = [top(1)+1,top(2)-1];
-        end
+    if ( probs(x,y) <= p )
+      map(x,y) = 0;
+      #top
+      if (y > 1 && map(x,y-1) == 0.5)
+        current(end+1,:) = [x,y-1];
       end
 
-      if (top(1) > 1)
-        current(end+1,:) = [top(1)-1,top(2)];
+      # Left and right
+      if (x > 1 && map(x-1,y) == 0.5)
+        current(end+1,:) = [x-1,y];
       end
-      if (top(1) < size*2+1)
-        current(end+1,:) = [top(1)+1,top(2)];
+      if (x < size*2+1 && map(x+1,y) == 0.5)
+        current(end+1,:) = [x+1,y];
       end
 
-      if (top(2) < size*2+1)
-        if (top(1) > 1)
-          current(end+1,:) = [top(1)-1,top(2)+1];
-        end
-        current(end+1,:) = [top(1),top(2)+1];
-        if (top(1) < size*2+1)
-          current(end+1,:) = [top(1)+1,top(2)+1];
-        end
+      # bottom
+      if (y < size*2+1 && map(x,y+1) == 0.5)
+        current(end+1,:) = [x,y+1];
       end
+      
 
     else
       # This field is empty
-      map(top(1),top(2)) = 1.0;
+      map(x,y) = 1.0;
     end
-
+    # Node not visited
   end
 
 end
--- ass2/plotcluster.m
+++ ass2/plotcluster.m
@@ -1,6 +1,5 @@
 function s = plotcluster(p, size)
 
-cluster = gencluster(p,size)
+cluster = gencluster(p,size);
 img = gray2ind(cluster);
-
-s=imshow(img,[0,1.1]);
+imshow(img,[0,64]);
